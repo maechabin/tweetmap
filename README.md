88fb9660537f7b0154ed909295d9b4112729c60c
maeda <takanori@stroly.jp>
2019-09-21 19:02:17 +0900

    title変更

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Tweetmap
+# angular-leaflet-boilerplate
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 8.3.2.
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'tweetmap'`, () => {
+  it(`should have as title 'angular-leaflet-boilerplate'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('tweetmap');
+    expect(app.title).toEqual('angular-leaflet-boilerplate');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('tweetmap app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('angular-leaflet-boilerplate app is running!');
   });
 });
